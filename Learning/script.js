39be7d03136dbc5e621cad1056607aaaf439d540
Harikrishnan <harikrishnankannoth@gmail.com>
2022-10-28 21:00:08 +0530

    Simple methods in JS

--- Learning/script.js
+++ Learning/script.js
@@ -1,1 +1,11 @@
 //Arrays
+const numbers = ['apples','1','howdo',1,3];
+numbers[5] = "Hoduu";
+numbers.push("mangos")
+numbers.unshift("straw");
+
+
+numbers.pop();
+console.log(numbers);
+
+console.log(Array.isArray(numbers))
