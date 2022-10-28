c4e460e908007b48392af7903fb72d63247ff9ad
Harikrishnan <harikrishnankannoth@gmail.com>
2022-10-29 01:12:27 +0530

    Loops and Objects in JS

--- Learning/script.js
+++ Learning/script.js
@@ -9,3 +9,65 @@ numbers.pop();
 console.log(numbers);
 
 console.log(Array.isArray(numbers))
+
+
+
+
+//Object literals
+
+const person = {
+    fistName: "John",
+    secondName: "Doe",
+    age:30,
+    movies: ["Spiderman", "Ironman"],
+    address: {
+        street: "221 B",
+        city : "Kandy",
+        State : "Emergency"
+    }
+}
+
+person.email ="john@gmail.com"
+
+console.log(person);
+console.log(person.fistName);
+
+
+//DESTRUCTURING
+
+const {fistName, secondName, address:{city}} = person;
+console.log(fistName);
+console.log(city);
+
+//
+const todos = [
+    {
+        id: 1,
+        work: "Go to College",
+        complete: false
+    },
+    {
+        id: 2,
+        work: "Buy Food",
+        complete: false
+    },
+    {
+        id: 3,
+        work: "Clean Room",
+        complete: true
+    }
+];
+
+console.log(todos[1].work);
+
+//Converting to JSON
+const todosJSON = JSON.stringify(todos);
+console.log(todosJSON);
+
+
+//loops
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+
